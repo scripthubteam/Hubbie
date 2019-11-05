5b8927e9f40f8877bc32ac8375df49f1f6a2fd3d
tati <mazzasantino1206@gmail.com>
2019-11-05 20:32:20 -0300

    Se implemento mongoose en el comando invite

--- comandos/invite.js
+++ comandos/invite.js
@@ -1,165 +1,162 @@
-const Discord = require("discord.js");
-const db = require("../db/db.js");
-let Reg = db.loadRegHelper(),
-    BotStorage_,
-    Global;
+const botSchema = require("../models/botSchema")
+const queueSchema = require("../models/queueSchema")
 
-const { inviteChan, staffBotReqChan } = require("../chans.json")
-
-Reg.init("BotStorage_", "{}");
-
-if (typeof BotStorage_ === 'undefined') {
-    BotStorage_ = {};
-    try {
-        BotStorage_ = JSON.parse(Reg.get("BotStorage_"));
-    } catch (e) {
-        BotStorage_ = {};
-    }
-}
-
-Reg.init("Global", "{}");
-
-if (typeof Global === 'undefined') {
-    Global = {};
-    try {
-        Global = JSON.parse(Reg.get("Global"));
-    } catch (e) {
-        Global = {};
-    }
-}
+const {
+  inviteChan,
+  staffBotReqChan
+} = require("../chans.json")
 
 exports.run = async (bot, msg, args) => {
 
-    if (msg.channel.id !== inviteChan) {
-        return;
-    }
-    
-    if (isNaN(args[0])) {
-        msg.channel.send(":x: **Esa no es una ID válida.** La ID debe contener el número del cliente de la apliación/usuario.");
-        return;
-    }
+  if (msg.channel.id !== inviteChan) {
+    return;
+  }
 
-    if(args[0] === bot.user.id){
-        msg.channel.send({
-          content: "Sí, claro.",
-          files: ["https://i.kym-cdn.com/photos/images/original/001/044/374/2f0.png"]
-        })
-        return;
-      }
-    
-    if (!args[1]) {
-        msg.channel.send(":x: **Por favor, especifica un prefix para tu bot.** El prefix ayudará a saber cómo se ejecuta tu bot.");
-        return;
-    }
 
-    var user = await bot.fetchUser(args[0]);
+  if (isNaN(args[0])) {
+    msg.channel.send(":x: **Esa no es una ID válida.** La ID debe contener el número del cliente de la apliación/usuario.");
+    return;
+  }
 
-    if(!user.bot){
-        msg.channel.send(":x: **La ID introducida no pertenece a un Bot**.")
-        return;
-    }
+  let user = await bot.fetchUser(args[0])
+  let dbBot = await botSchema.findOne({
+    botId: user.id
+  });
 
-    var dbBot = await BotStorage_[user.id];
+  if (args[0] === bot.user.id) {
+    msg.channel.send({
+      content: "Sí, claro.",
+      files: ["https://i.kym-cdn.com/photos/images/original/001/044/374/2f0.png"]
+    })
+    return;
+  }
 
-    if (dbBot !== undefined) {
-        if(dbBot.data.appr.isAppr === true && dbBot.owner.id === msg.author.id){
-            msg.channel.send(":x: **Este bot ya está aprobado.** Y es de tu propiedad, ¿sucede algo?");
-            return;
-        }
+  if (!args[1]) {
+    msg.channel.send(":x: **Por favor, especifica un prefix para tu bot.** El prefix ayudará a saber cómo se ejecuta tu bot.");
+    return;
+  }
 
-        if (dbBot.data.appr.isQueued === true && dbBot.owner.id === msg.author.id) {
-            msg.channel.send(":x: **Este bot está en la cola de espera. Número: `"+BotStorage_[user.id].data.appr.nQueue+"`**");
-            return;
-        }
+
+  if (!user.bot) {
+    msg.channel.send(":x: **La ID introducida no pertenece a un Bot**.")
+    return;
+  }
+
+
+  if (dbBot !== null) {
+    if (dbBot.isAppr && dbBot.ownerId === msg.author.id) {
+      msg.channel.send(":x: **Este bot ya está aprobado.** Y es de tu propiedad, ¿sucede algo?");
+      return;
     }
 
-    if (!BotStorage_[user.id]) BotStorage_[user.id] = {
-      "data": {
-        "id": user.id,
-        "request": {
-          "day": Date.now()
-        },
-        "appr": {
-          "isAppr": false,
-          "isQueued": true,
-          "nQueue": 0,
-          "day": 0
-        }
+    if (dbBot.isQueued && dbBot.ownerId === msg.author.id) {
+      msg.channel.send(":x: **Este bot está en la cola de espera. Número: `" + dbBot.nQueue + "`**");
+      return;
+    }
+  }
+
+  if (dbBot === null) {
+    dbBot = await botSchema.create({
+      botId: user.id,
+      requested: Date.now(),
+      isAppr: false,
+      isQueued: true,
+      nQueue: 0,
+      day: 0,
+      ownerId: msg.author.id,
+      prefix: args[1],
+      info: "Beep boop, beep?",
+      certified: false,
+      votes_plus: 0,
+      votes_negative: 0,
+      vote_players: []
+    })
+  }
+
+  let queueDb = await queueSchema.findOne({
+    serverId: msg.guild.id
+  })
+
+  if (queueDb === undefined || queueDb === null) {
+    await queueSchema.create({
+      serverId: msg.guild.id,
+      globalQueued: 0
+    })
+    queueDb = await queueSchema.findOne({
+      serverId: msg.guild.id
+    })
+  };
+
+
+  await queueSchema.findOneAndUpdate({
+    serverId: msg.guild.id
+  }, {
+    globalQueued: queueDb.globalQueued + 1
+  })
+
+  let count = await queueSchema.findOne({
+    serverId: msg.guild.id
+  })
+
+  await botSchema.findOneAndUpdate({
+    botId: user.id
+  }, {
+    nQueue: count.globalQueued
+  })
+
+  const embed_dm = {
+    "embed": {
+      "color": msg.member.roles.first().color,
+      "timestamp": new Date,
+      "footer": {
+        "text": "Equipo de Aprobación de Aplicaciones"
       },
-      "owner": {
-        "id": msg.author.id
+      "image": {
+        "url": "https://i.imgur.com/D56tkxB.png"
       },
-     "config": {
-        "prefix": args[1],
-        "info": "Beep boop, beep?",
-        "certified": false,
-        "votes_plus": 0,
-        "votes_negative": 0,
-        "vote_players": []
-      }
+      "fields": [{
+        "name": "Su solicitud está pendiente de aprobación.",
+        "value": "Le notificamos que su solicitud fue hecha el día **" + new Date + "** y está pendiente de aprobarse o ser rechazada a la brevedad. Su puesto en la cola de espera: `" + dbBot.nQueue + "` ¡Sea paciente!"
+      }]
     }
-    if (!Global[msg.guild.id] || Global[msg.guild.id] == null) Global[msg.guild.id] = { q: 0 };
-    Reg.save("Global", JSON.stringify(Global));
+  }
 
-    var count = Global[msg.guild.id].q += 1;
-    Reg.save("Global", JSON.stringify(Global));
-    BotStorage_[user.id].data.appr.nQueue += Number(count)
-    Reg.save("BotStorage_", JSON.stringify(BotStorage_));
-
-    const embed_dm = {"embed": {
-        "color": msg.member.roles.first().color,
-        "timestamp": new Date,
-        "footer": {
-          "text": "Equipo de Aprobación de Aplicaciones"
-        },
-        "image": {
-          "url": "https://i.imgur.com/D56tkxB.png"
-        },
-        "fields": [
-          {
-            "name": "Su solicitud está pendiente de aprobación.",
-            "value": "Le notificamos que su solicitud fue hecha el día **"+new Date+"** y está pendiente de aprobarse o ser rechazada a la brevedad. Su puesto en la cola de espera: `"+BotStorage_[user.id].data.appr.nQueue+"` ¡Sea paciente!"
-          }
-        ]
-      }
-    }
-
-    const embed_staffinv = {
-      "content": ":information_source: Para rechazar una solicitud, utilice `s!denegar <ID>`",
-      "embed": {
-        "title": "Solicitud de invitación",
-        "description": "El usuario **"+msg.author.tag+"** ha solicitado que su bot **"+user.tag+"** sea invitado a **Script Hub**",
-        "color": msg.member.roles.first().color,
-        "timestamp": new Date(),
-        "footer": {
-          "icon_url": msg.author.displayAvatarURL,
-          "text": "Detalles de la petición"
-        },
-        "thumbnail": {
-          "url": user.displayAvatarURL
-        },
-        "fields": [
-          {
-            "name": "🤖 Bot",
-            "value": user.tag+ " [INVITAR](https://discordapp.com/api/oauth2/authorize?client_id="+args[0]+"&permissions=104187968&scope=bot&guild_id=606199542605414428) - `ID: "+user.id+"`",
-            "inline": true
-          },
-          {
-            "name": "ℹ Prefix",
-            "value": "`"+args[1]+"`",
-            "inline": true
-          },
-          {
-            "name": "💻 Desarrollador",
-            "value": msg.author.tag+" `(Cuenta creada: "+new Date(msg.author.createdTimestamp)+")`",
-            "inline": true
-          }
-        ]
-      }
-    }
-    const embed_botinv = {"embed": {
+  const embed_staffinv = {
+    "content": ":information_source: Para rechazar una solicitud, utilice `s!denegar <ID>`",
+    "embed": {
       "title": "Solicitud de invitación",
-      "description": "¡Hola **"+msg.author.tag+"**! Gracias por invitar tu bot a **Script Hub**, este será en lo más breve posible introducido al servidor si cumple los requisitos. Te enviaremos una notificación cuando se decida sobre tu solicitud.",
+      "description": "El usuario **" + msg.author.tag + "** ha solicitado que su bot **" + user.tag + "** sea invitado a **Script Hub**",
+      "color": msg.member.roles.first().color,
+      "timestamp": new Date(),
+      "footer": {
+        "icon_url": msg.author.displayAvatarURL,
+        "text": "Detalles de la petición"
+      },
+      "thumbnail": {
+        "url": user.displayAvatarURL
+      },
+      "fields": [{
+          "name": "🤖 Bot",
+          "value": user.tag + " [INVITAR](https://discordapp.com/api/oauth2/authorize?client_id=" + args[0] + "&permissions=104187968&scope=bot&guild_id=606199542605414428) - `ID: " + user.id + "`",
+          "inline": true
+        },
+        {
+          "name": "ℹ Prefix",
+          "value": "`" + args[1] + "`",
+          "inline": true
+        },
+        {
+          "name": "💻 Desarrollador",
+          "value": msg.author.tag + " `(Cuenta creada: " + new Date(msg.author.createdTimestamp) + ")`",
+          "inline": true
+        }
+      ]
+    }
+  }
+  const embed_botinv = {
+    "embed": {
+      "title": "Solicitud de invitación",
+      "description": "¡Hola **" + msg.author.tag + "**! Gracias por invitar tu bot a **Script Hub**, este será en lo más breve posible introducido al servidor si cumple los requisitos. Te enviaremos una notificación cuando se decida sobre tu solicitud.",
       "color": msg.member.roles.first().color,
       "timestamp": new Date(),
       "footer": {
@@ -169,15 +166,14 @@ exports.run = async (bot, msg, args) => {
       "thumbnail": {
         "url": user.displayAvatarURL
       },
-      "fields": [
-        {
+      "fields": [{
           "name": "🤖 Bot",
           "value": user.tag,
           "inline": true
         },
         {
           "name": "ℹ Prefix",
-          "value": "`"+args[1]+"`",
+          "value": "`" + args[1] + "`",
           "inline": true
         },
         {
@@ -187,9 +183,9 @@ exports.run = async (bot, msg, args) => {
         }
       ]
     }
-    }
+  }
 
-    bot.channels.get(inviteChan).send(embed_botinv)
-    msg.author.send(embed_dm)
-    bot.channels.get(staffBotReqChan).send(embed_staffinv)
+  bot.channels.get(inviteChan).send(embed_botinv)
+  msg.author.send(embed_dm)
+  bot.channels.get(staffBotReqChan).send(embed_staffinv)
 };
