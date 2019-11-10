2632705b3dd9a29f48edc2e0fbf285dee5fda9a9
Devsaider <saskexgames@gmail.com>
2019-11-10 10:20:44 -0600

    git command

--- comandos/git.js
+++ comandos/git.js
@@ -1,5 +1,5 @@
 exports.run = async (bot, msg, args) => {
-    var exec = require('child_process').exec, git;
+    var exec = require('child_process').exec, git, refresh;
     if (!msg.member.hasPermission("MANAGE_GUILD")) return msg.channel.send(":x: No posees los permisos necesarios.")
     if(!args[0]){
         message.channel.send(":x: **Falta:** branch (rama)");
@@ -10,10 +10,21 @@ exports.run = async (bot, msg, args) => {
         console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
-             message.channel.send(":x: **Error:** "+ error);
+             message.channel.send(":x: **Error:** No se pudo contactar a la rama. "+ error);
              return;
         }
     });
     git();
+    refresh = exec('refresh',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+             message.channel.send(":x: **Error:** No se pudo refrescar los cambios. "+ error);
+             return;
+        }
+    });
+    refresh();
     
 }
