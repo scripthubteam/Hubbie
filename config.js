911e6951bf44b03599a7212d2acb9bb274a76b64
Glitch (scripthub-bot-definitivo) <none>
2019-11-11 22:06:01 +0000

    delete config.json

--- config.js
+++
@@ -1,2 +0,0 @@
-process.env.MONGOURI = "mongodb://localhost/dbot"
-process.env.TOKEN = "NjM2Njk1MTc5MjY4NDU2NDU5.XcmFVA.gRXN7MJt5QebWDrwLVA2LJm8X18"
--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const express = require("express");
 const app = express();
 
-require("./config")
-
 app.get("*", (req, res) => {
   res.send("OK");
 });
