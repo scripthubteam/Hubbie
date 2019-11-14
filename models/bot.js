1f7cada3c37ca91f4cb13cf16869aabeec418041
Devsaider <saskexgames@gmail.com>
2019-11-14 14:17:53 -0600

    fix Schema

--- models/bot.js
+++ models/bot.js
@@ -1,9 +1,11 @@
-const { Schema, model } = require("mongoose");
+onst { Schema, model } = require("mongoose");
 
 const botSchema = new Schema({
+  ownerId: {
   _id: {
     type: String
   },
+  nQueue: {
   idOwner: {
     type: String
   },
@@ -11,23 +13,31 @@ const botSchema = new Schema({
     type: Number
   },
   prefix: {
-    type: String
-  },
-  approvedDate: {
     type: Number,
     default: 0
   },
+  info: {
   description: {
     type: String,
     default: "Un bot simple"
   },
+  certified: {
   verified: {
     type: Boolean,
     default: false
   },
+  requested:{
+    type: Number
+  },
   votes: {
     type: Array,
     default: []
+  },
+  votes_plus:{
+    type: Number
+  },
+  votes_neegative:{
+    type: Number
   }
 });
 
