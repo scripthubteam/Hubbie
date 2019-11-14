a7fde29c32233af3bb0abb5b6e3a070b3b681da0
tati <mazzasantino1206@gmail.com>
2019-11-14 18:31:13 -0300

    Se editó el esquema

--- models/bot.js
+++ models/bot.js
@@ -1,40 +1,43 @@
 const { Schema, model } = require("mongoose");
 
 const botSchema = new Schema({
-  ownerId: {
-    type: String
-  },
-  nQueue: {
-    type: Number
-  },
-  prefix: {
-    type: String
-  },
-  approvedDate: {
-    type: Number,
-    default: 0
-  },
-  info: {
-    type: String,
-    default: "Un bot simple"
-  },
-  certified: {
-    type: Boolean,
-    default: false
-  },
-  requested:{
-    type: Number
-  },
-  votes: {
-    type: Array,
-    default: []
-  },
-  votes_plus:{
-    type: Number
-  },
-  votes_neegative:{
-    type: Number
-  }
+    ownerId: {
+        type: String
+    },
+    botId: {
+        type: String
+    },
+    nQueue: {
+        type: Number
+    },
+    prefix: {
+        type: String
+    },
+    approvedDate: {
+        type: Number,
+        default: 0
+    },
+    info: {
+        type: String,
+        default: "Un bot simple"
+    },
+    certified: {
+        type: Boolean,
+        default: false
+    },
+    requested: {
+        type: Number
+    },
+    votes: {
+        type: Array,
+        default: []
+    },
+    votes_plus: {
+        type: Number
+    },
+    votes_neegative: {
+        type: Number
+    }
 });
 
 exports.bots = model("bots", botSchema);
