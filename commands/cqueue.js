ae7e13162a9394ab790f95fa9c77e6b4a8774900
tati <mazzasantino1206@gmail.com>
2019-11-24 02:02:26 -0300

    Se implementó la clase BotManager en el comando cqueue

--- commands/cqueue.js
+++ commands/cqueue.js
@@ -1,17 +1,22 @@
+const BotManager = require('../lib/BotManager');
+const botManager = new BotManager();
+
 exports.run = async (client, msg, args) => {
   // Verifica si el usuario pertenece al personal del servidor.
   if (!msg.member.hasPermission('MANAGE_GUILD')) return msg.channel.send(':x: No posees los permisos necesarios.');
 
   // Se obtienen los bots en espera y se verifica si hay más de uno.
-  const queue = await client.db.bots.find({approvedDate: 0}).exec();
-  if (queue.length < 1) return msg.channel.send(':x: **No hay ningún bot en la lista de espera**.');
+  if (await botManager.getQueue() === 0) return msg.channel.send(':x: **No hay ningún bot en la lista de espera**.');
+  const botsInQueue = await botManager.getNoApprovedBots();
 
   // Se define una variable de salida y se agrega cada bot por orden de lista.
   let out = 'Lista de espera:\n';
-  queue
-      .sort((a, b) => a.nQueue - b.nQueue)
+  botsInQueue
+      .sort()
       .forEach((bot) => {
-        out += `> **${bot.nQueue}**. ${msg.guild.members.get(bot.botId).user.tag} - ${msg.guild.members.get(bot.ownerId).user.tag} - \`${bot.prefix}\`\n`;
+        const memberBot = msg.guild.members.get(bot.botId);
+        const memberOwner = msg.guild.members.get(bot.ownerId);
+        out += `> **${bot.nQueue}**. ${memberBot.user.tag} - ${memberOwner.user.tag} - \`${bot.prefix}\`\n`;
       });
 
   // Se envía la variable que contiene la lista de bots en espera.
