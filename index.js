f909c1edfe2fb9d1924326a54dc2bfdabe0b7a4e
tati <mazzasantino1206@gmail.com>
2019-11-24 01:02:03 -0300

    Se eliminaron cosas inecesarias

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
-//dotenv
-require('dotenv').config()
+// dotenv
+require('dotenv').config();
 // Iniciamos un servidor web en el puerto por defecto para que Glitch no detecte errores.
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("*", (req, res) => {
-    res.send("No quedará en la noche una estrella.\nNo quedará la noche.\nMoriré y conmigo la suma\ndel intolerable universo.\nBorraré las pirámides, las medallas,\nlos continentes y las caras.\nBorraré la acumulación del pasado.\nHaré polvo la historia, polvo el polvo.\nEstoy mirando el último poniente.\nOigo el último pájaro.\nLlego la nada a nadie.\n");
+app.get('*', (req, res) => {
+  res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 3000, (e) => {
-            console.log(`${e ? `${e.toString()}${e.fileName ? ` - ${e.fileName}:${e.lineNumber}:${e.columnNumber}` : ``}` : "Página web lista!"}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`'Pagina web lista`);
 });
 
 // Código del bot en sí.
-const Discord = require("discord.js");
-const mongoose = require("mongoose");
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
 const client = new Discord.Client();
-const moment = require('moment');
-const path = require("path");
-const fs = require("fs");
-const log = (message) => {
-  console.log(`[${moment().format("MM-DD-YYYY HH:mm:ss")}] ${message}`);
-};
-
-// Bot Modules
-client.errorLog = require("./bot_modules/errorLog.js");
+const path = require('path');
 
 /*
-* Usaremos una funcion para iniciar al bot de manera sincronizada para evitar posibles fallas 
-*/
+ * Usaremos una funcion para iniciar al bot de manera sincronizada para evitar posibles fallas
+ */
 
-const loadCommands = require('./bot_modules/loadCommands')
-const loadEvents = require('./bot_modules/loadEvents')
+const loadCommands = require('./lib/loadCommands');
+const loadEvents = require('./lib/loadEvents');
 
-const init = async () => {
-
+async function init() {
   // Conectando a base de datos MongoDB.
   await mongoose.connect(process.env.MONGOURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (e) => {
-    console.log(`${e ? `${e.toString()}${e.fileName ? ` - ${e.fileName}:${e.lineNumber}:${e.columnNumber}` : ``}` : `Base de datos lista!`}`);
-  }).catch(e => {
-    throw new Error('No se pudo realizar la conexion a la base de datos')
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }, (err) => {
+    if (err) {
+      console.error(err.toString());
+    }
+    console.log(`${err ? `${err.toString()}${err.fileName ? ` - ${err.fileName}:${err.lineNumber}:${err.columnNumber}` : ``}` : `Base de datos lista!`}`);
+  }).catch((err) => {
+    console.error(err);
   });
 
-
   client.onlyDeleteUsers = [];
-  client.db = require("./models/bot.js");
   client.cmds = new Discord.Collection();
   client.aliases = new Discord.Collection();
 
-  //Usaremos las funciones loadEvents y loadCommands para iniciar el bot de manera sincronizada
-  await loadEvents(path.resolve(__dirname, 'events'), client)
-  await loadCommands(path.resolve(__dirname, 'cmds'), client)
-
- // Iniciamos sesión en el bot con su token correspondiente y si hay algún error lo muestra.
-  client.login(process.env.TOKEN).catch((e) => {
-   errorLog(e);
-   console.error(`${e.toString()}${e.fileName ? ` - ${e.fileName}:${e.lineNumber}:${e.columnNumber}` : ``}`);
-  });
-}
+  await loadEvents(path.join(__dirname, 'events'), client);
+  await loadCommands(path.join(__dirname, 'commands'), client);
+  client.login(process.env.TOKEN)
+      .catch((err) => {
+        console.error(err);
+      });
+};
 
 // Controlamos todas las excepciones recibidas que no han sido controladas anteriormente.
-process.on("unhandledRejection", e => {
-  // Se envía todo eso en la consola.
-  client.errorLog(e);
-  console.error(`${e.toString()}${e.fileName ? ` - ${e.fileName}:${e.lineNumber}:${e.columnNumber}` : ``}`);
+process.on('unhandledRejection', (err) => {
+  console.error(err);
 });
 
-init()
+init();
