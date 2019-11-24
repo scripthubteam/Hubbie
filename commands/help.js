654ce8c05925c64a288815832799a4cb53676344
tati <mazzasantino1206@gmail.com>
2019-11-24 02:52:11 -0300

    Se mejoró el codigo del comando help

--- commands/help.js
+++ commands/help.js
@@ -8,14 +8,10 @@ exports.run = async (client, msg, args) => {
       .setDescription('**Script Hub Bot** es uno de los proyectos **open source** de Script Hub que es publicado gracias al equipo de desarrolladores sin ánimos de lucro de **Script Hub OTA**. \n<:scripthub:646177532365897730> [¡Contribuye libremente con nosotros y aprende!](https://github.com/scripthubteam)')
       .setThumbnail('https://i.imgur.com/xtkteHF.png');
 
-  const embed = new RichEmbed()
-      .setAuthor(client.user.username, client.user.displayAvatarURL)
-      .setColor(0x36393e)
-      .setFooter(msg.author.tag, msg.author.displayAvatarURL);
 
   // Definimos una variable de salida y por cada comando público, lo listamos en esa variable para ser colocado en el Embed.
   let outPublic = '';
-  client.cmds.filter((cmd) => cmd.public).array().forEach((cmd) => {
+  client.cmds.filter((cmd) => cmd.public).forEach((cmd) => {
     outPublic += `**[${cmd.usage.trim()}](https://scripthubteam.github.io/)** - ${cmd.description.trim()}\n`;
   });
 
@@ -27,30 +23,25 @@ exports.run = async (client, msg, args) => {
       outPrivate += `**[${cmd.usage.trim()}](https://scripthubteam.github.io/)** - ${cmd.description.trim()}\n`;
     });
 
+    const embedServer = new RichEmbed()
+        .setTitle('Lista de comandos')
+        .setDescription(outPublic)
+        .setAuthor(client.user.username, client.user.displayAvatarURL)
+        .setColor(0x36393e)
+        .setFooter(msg.author.tag, msg.author.displayAvatarURL);
     // Se coloca un título y descripción diferente para los privados.
-    embed
+    const embedPrivate = new RichEmbed()
         .setTitle('Panel de Administración')
-        .setDescription(outPrivate);
+        .setDescription(outPrivate)
+        .setAuthor(client.user.username, client.user.displayAvatarURL)
+        .setColor(0x36393e)
+        .setFooter(msg.author.tag, msg.author.displayAvatarURL);
 
-    // Se envía el Embed con los comandos privados.
-    msg.author.send(embed).catch((e) => {
-      console.error(e);
-    });
-  }
-
-  // Se coloca la descripción del Embed con la información de comandos públicos.
-  embed.setTitle('Lista de comandos');
-  embed.setDescription(outPublic);
-
-  // Se envía el Embed con los comandos públicos.
-  msg.channel.send(embed).catch((e) => {
-    console.error(e);
-  });
-  msg.channel.send(embedOTA).catch((e) => {
-    console.error(e);
-  });
+    msg.author.send(embedPrivate);
+    msg.channel.send(embedServer);
+    msg.channel.send(embedOTA);
+  };
 };
-
 exports.aliases = [];
 exports.public = false;
 exports.description = 'Muestra la ayuda.';
