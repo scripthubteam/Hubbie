915af8647e4693c192da471f2155cb0ebff1a8b7
tati <mazzasantino1206@gmail.com>
2019-11-24 01:02:36 -0300

    Se borró la carpeta cmds y la carpeta boy_modules se renombró a lib

--- cmds/ping.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (client, msg, args) => {
-  // Envía la latencia actual en milisegundos.
-  msg.channel.send(`Pong! ${Math.floor(client.ping)}ms.`);
-}
-
-exports.aliases = [];
-exports.public = false;
-exports.description = "Muestra la latencia en milisegundos.";
-exports.usage = "s!ping";
