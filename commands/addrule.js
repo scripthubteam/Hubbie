5b0398cd66720e76650db710c254e103d06d29f5
Lauuu <elkozi116@gmail.com>
2019-12-01 12:22:51 -0300

    Addrule y announce

--- commands/addrule.js
+++ commands/addrule.js
@@ -1,10 +1,12 @@
-const { ruleChannel } = require("../channelsConfig.json.js");
+require('dotenv').config();
+const ruleChannel = process.env.ruleChannel
 const Discord = require("discord.js");
 
 exports.run = async (client, msg, args) => {
 
+    let intRule = 0;
     args = args.join(" ").split("|").map(arg => arg.trim());
-    if (!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send(":x: No posees los permisos necesarios.");
+    if (!msg.member.hasPermission("MANAGE_GUILD")) return msg.channel.send(":x: No posees los permisos necesarios.");
 
     if (!args[0]) {
         msg.channel.send(":x: Debes especificar una título para la regla a añadir.");
@@ -12,13 +14,20 @@ exports.run = async (client, msg, args) => {
     }
 
     if (!args[1]) {
-        args[1] = "No se especifico una descripción para esta regla."
+        msg.channel.send(":x: Debes especificar una descripción para la regla a añadir.");
+        return;
     }
 
     let embed = new Discord.RichEmbed()
     .setTitle(args[0])
     .setDescription(args[1])
-    .setFooter("Regla #"+aquíirialadb)
+    .setFooter("Regla #"+intRule+1)
+    .setColor("RED")
     client.channels.get(ruleChannel).send(embed)
 
 }
+
+exports.aliases = [];
+exports.public = false;
+exports.description = 'Añade una regla al respectivo canal de reglas.';
+exports.usage = 's!addrule ';
