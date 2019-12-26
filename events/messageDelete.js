93ddce37eed5de9709c1f18a41f731030a52e031
Devsaider <saskexgames@gmail.com>
2019-12-26 16:43:07 -0600

    Comando team.js y cambios en los logs.

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,29 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 const privateLogsChannelId = process.env.privateLogsChannelId;
-const {RichEmbed} = require('discord.js');
+const { RichEmbed } = require("discord.js");
 
 module.exports = async (client, msg) => {
   // Se crea un Embed base con el autor del mensaje.
   const embed = new RichEmbed()
-      .setDescription('Mensaje borrado en ' + msg.channel.toString())
-      .addField('> Usuario:', `<@${msg.author.id}> \`(${msg.author.id}\`)`);
+    .setDescription("Mensaje borrado en " + msg.channel.toString())
+    .addField("> Usuario:", `<@${msg.author.id}> \`(${msg.author.id}\`)`);
 
   // Si hay mensaje o el mensaje es mayor a uno, lo coloca en el Embed.
   if (msg.content && msg.content.length > 1) {
-    embed.addField('> Mensaje:', msg.content);
+    embed.addField("> Mensaje:", msg.content);
   }
 
   // Si hay archivos adjuntos en el mensaje se colocan.
   if (msg.attachments.size > 0) {
     // Se colocan de la siguiente forma [archivo.adjunto](url), [archivo](url).
-    const urls = msg.attachments.map((r) => `[${r.filename}](https://media.discordapp.net/attachments/${msg.channel.id}/${r.id}/${r.filename})`).join(', ');
+    const urls = msg.attachments
+      .map(
+        r =>
+          `[${r.filename}](https://media.discordapp.net/attachments/${msg.channel.id}/${r.id}/${r.filename})`
+      )
+      .join(", ");
 
     // Se agregan al Embed.
-    embed.addField('> Archivos:', `${urls}`);
+    embed.addField("> Archivos:", `${urls}`);
   }
 
   // Se envía el Embed al registro privado.
-  client.channels.get(privateLogsChannelId).send(embed).catch((e) => {
-    console.error(e);
-  });
+  client.channels
+    .get(privateLogsChannelId)
+    .send(embed)
+    .catch(e => {
+      console.error(e);
+    });
 };
