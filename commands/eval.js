db844c366cdb040f7a789d09026b5e786fcfcbbc
wwmon <mizuesmuygay@gmail.com>
2019-12-28 09:36:09 -0600

    cambios en logs y eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,45 +1,42 @@
-const { RichEmbed } = require("discord.js");
+require('dotenv').config();
+const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, msg, args) => {
-  const arr = {
-    arr: [
-      "595734746059898927",
-      "280985817097306113",
-      "506199865722798092",
-      "608817245417898004"
-    ]
-  };
-  if (arr.arr.indexOf(msg.author.id) != -1) {
-    const clean = text => {
-      if (typeof text === "string")
-        return text
-          .replace(/`/g, "`" + String.fromCharCode(8203))
-          .replace(/@/g, "@" + String.fromCharCode(8203))
-          .substr(0, 1014);
-      else {
-        return text;
-      }
-    };
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-      if (typeof evaled !== "string");
-      evaled = require("util").inspect(evaled);
-      console.log(clean(evaled));
-      const embed = new RichEmbed()
-        .setColor(0xf4427a)
-        .setFooter("Eval")
-        .addField("INPUT", `\`\`\`js\n${code}\n\`\`\``)
-        .addField("OUTPUT", `\`\`\`js\n${clean(evaled)}\n\`\`\``);
-      msg.channel.send(embed);
-    } catch (err) {
-      msg.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-      console.error(err);
-    }
-  }
+	let arr = [
+		'595734746059898927',
+		'280985817097306113',
+		'506199865722798092',
+		'608817245417898004'
+	];
+	if (!arr.includes(msg.author.id)) return;
+	else {
+		let author = message.author,
+			member = message.member,
+			guild = message.guild,
+			channel = message.channel,
+			client = message.client,
+			message = msg;
+		try {
+			let evalued = await eval(args.join(' '));
+			if (typeof evalued !== 'string')
+				evalued = util.inspect(evalued, { depth: 0 });
+			if (evalued.length > 1950) {
+				message.channel.send('> Error: El resultado es muy largo');
+			} else if (evalued.includes(client.token || process.env.MONGOURI)) {
+				message.channel.send('> Error: El resultado contiene un token');
+			} else {
+				message.channel.send('> Hecho:\n```js\n' + evalued + '\n```');
+			}
+		} catch (err) {
+			err = util.inspect(err, { depth: 0 });
+			if (err.includes(client.token || process.env.MONGOURI))
+				err = err.replace(client.token || process.env.MONGOURI, 'T0K3N');
+			message.channel.send('> Error: \n```js\n' + err + '\n```');
+		}
+	}
 };
 
-exports.aliases = ["e", "chk"];
+exports.aliases = ['e', 'chk'];
 exports.public = false;
-exports.description = "Evalúa código.";
-exports.usage = "s!eval código";
+exports.description = 'Evalúa código.';
+exports.usage = 's!eval código';
--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,37 +1,35 @@
-require("dotenv").config();
+require('dotenv').config();
 const privateLogsChannelId = process.env.privateLogsChannelId;
-const { RichEmbed } = require("discord.js");
+const { RichEmbed } = require('discord.js');
 
 module.exports = async (client, msg) => {
-  // Se crea un Embed base con el autor del mensaje.
-  const embed = new RichEmbed()
-    .setDescription("Mensaje borrado en " + msg.channel.toString())
-    .addField("> Usuario:", `<@${msg.author.id}> \`(${msg.author.id}\`)`);
+	// Se crea un Embed base con el autor del mensaje.
+	const embed = new RichEmbed()
+		.setColor(0xf04947)
+		.setTitle('<:shMiscTickError:653815214541766656> • Mensaje borrado en ' + msg.channel.name)
+		.addField('> Usuario:', `<@${msg.author.id}> \`(${msg.author.id})\``);
 
-  // Si hay mensaje o el mensaje es mayor a uno, lo coloca en el Embed.
-  if (msg.content && msg.content.length > 1) {
-    embed.addField("> Mensaje:", msg.content);
-  }
+	// Si hay mensaje lo coloca en el Embed.
+	if (msg.content) {
+		embed.addField('> Mensaje:', msg.content);
+	}
 
-  // Si hay archivos adjuntos en el mensaje se colocan.
-  if (msg.attachments.size > 0) {
-    // Se colocan de la siguiente forma [archivo.adjunto](url), [archivo](url).
-    const urls = msg.attachments
-      .map(
-        r =>
-          `[${r.filename}](https://media.discordapp.net/attachments/${msg.channel.id}/${r.id}/${r.filename})`
-      )
-      .join(", ");
+	// Si hay archivos adjuntos en el mensaje se colocan.
+	if (msg.attachments.size > 0) {
+		// Se colocan de la siguiente forma [archivo.adjunto](url), [archivo](url).
+		const urls = msg.attachments
+			.map(r => `[${r.filename}](https://media.discordapp.net/attachments/${msg.channel.id}/${r.id}/${r.filename})`)
+			.join(',\n');
 
-    // Se agregan al Embed.
-    embed.addField("> Archivos:", `${urls}`);
-  }
+		// Se agregan al Embed.
+		embed.addField('> Archivos:', `${urls}`);
+	}
 
-  // Se envía el Embed al registro privado.
-  client.channels
-    .get(privateLogsChannelId)
-    .send(embed)
-    .catch(e => {
-      console.error(e);
-    });
+	// Se envía el Embed al registro privado.
+	client.channels
+		.get(privateLogsChannelId)
+		.send(embed)
+		.catch(e => {
+			console.error(e);
+		});
 };
